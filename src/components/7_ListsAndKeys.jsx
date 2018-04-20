a8629e34ac9e3c90096bc0061f9862bd7faa26e5
Zoran Markovic <zoran.markovic@netcentric.biz>
2018-04-20 09:13:46 +0200

    Keys

--- src/components/7_ListsAndKeys.jsx
+++ src/components/7_ListsAndKeys.jsx
@@ -4,14 +4,28 @@ import React from 'react';
 // https://reactjs.org/docs/lists-and-keys.html
 
 // Component that accepts an array of numbers and outputs an unordered list of elements.
+function ListItem(props) {
+  // Correct! There is no need to specify the key here:
+  return <li>{props.value}</li>;
+}
 
 function NumberList(props) {
     const numbers=props.numbers;
-    const listItems = numbers.map((number) =>
-        <li key={number.toString()}>
-            {number}
-        </li>
-    );
+    // A good rule of thumb is that elements inside the map() call need keys.
+    const listItems = numbers.map((number) => (
+        {/*
+            <li key={number.toString()}> //Key should be specified inside the array.
+                {number}
+            </li>
+        */},
+        // Or like this if we use another component -> Extracting Components with Keys
+        <ListItem
+            key={number.toString()}
+            value={number}
+        />
+
+    ));
+
     return (
         <ul>{listItems}</ul>
     );
@@ -36,3 +50,17 @@ export class List extends React.Component {
    }
 }// end of CLASS
 
+// Keys
+// Keys help React identify which items have changed, are added, or are removed.
+// Keys should be given to the elements inside the array to give the elements a stable identity:
+
+// The best way to pick a key is to use a string that uniquely identifies a list item among its siblings.
+// Most often you would use IDs from your data as keys:
+
+// const todoItems = todos.map((todo) =>
+//   <li key={todo.id}>
+//     {todo.text}
+//   </li>
+// );
+
+// For more info check DOCS!!!
